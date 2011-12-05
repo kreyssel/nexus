10c9404d2ebecb2e4598d77dd9018033c2b95421
Tamas Cservenak <tamas@cservenak.net>
2011-12-05 11:49:52 +0100

    Moved all of the feeds, feed recorder and timeline out to a plugin.
    To achieve this, following events were added:
    * real authc/authz events added, instead of old hald baked ones
    * validation related events added, now all validation emits events instead directly coupling to feedRecorder as before
    * task related events added, task do not couple to feed recorder anymore
    * other cleanup, like in security classes where the mess is endless it seems

--- nexus/nexus-core-plugins/nexus-timeline-plugin/src/test/java/org/sonatype/nexus/timeline/LegacyNexusTimelineTest.java
+++ nexus/nexus-core-plugins/nexus-timeline-plugin/src/test/java/org/sonatype/nexus/timeline/LegacyNexusTimelineTest.java
@@ -37,7 +37,7 @@ public class LegacyNexusTimelineTest
 
         FileUtils.copyDirectory( legacyDataDir, legacyTimelineDir );
 
-        NexusTimeline nexusTimeline = this.lookup( NexusTimeline.class, "real" );
+        NexusTimeline nexusTimeline = this.lookup( NexusTimeline.class );
 
         List<Entry> result = asList( nexusTimeline.retrieve( 0, 10, null, null, null ) );
 
@@ -60,7 +60,7 @@ public class LegacyNexusTimelineTest
 
         FileUtils.copyDirectory( newDataDir, newTimelineDir );
 
-        NexusTimeline nexusTimeline = this.lookup( NexusTimeline.class, "real" );
+        NexusTimeline nexusTimeline = this.lookup( NexusTimeline.class );
 
         List<Entry> result = asList( nexusTimeline.retrieve( 0, 10, null, null, null ) );
 
